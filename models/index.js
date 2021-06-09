31a07fad48971800da50a9c66fe7256d0352f089
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-09 13:58:46 -0500

    post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,11 @@
 const User = require('./User');
 const Post = require("./Post");
 const bcrypt = require('bcrypt');
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+
 module.exports = { User };
