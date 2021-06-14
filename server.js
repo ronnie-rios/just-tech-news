72fd4155ccce37076cda99dc455d610ea0f0687a
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-14 12:45:54 -0500

    added homepage for posts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const path = require('path');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -12,12 +12,13 @@ const PORT = process.env.PORT || 3001;
 app.engine('handlebars', hbs.engine);
 app.set('view engine','handlebars');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
-app.use(express.static(path.join(__dirname, 'public')));
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
