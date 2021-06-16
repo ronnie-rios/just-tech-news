7196f8da411c2dd0cf41acc786d9c8f57ffc73da
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-16 12:24:50 -0500

    updated upvote feature

---
+++ public/javascript/upvote.js
@@ -0,0 +1,9 @@
+async function upvoteClickHandler(event) {
+    event.preventDefault();
+  
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    console.log(id);
+  }
